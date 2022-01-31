4572bd193558de71e997a4864ed59a4dd4fe0cf3
Decha_L <blackneko1234@hotmail.com>
2022-01-31 12:42:28 +0700

    Anonymous login

--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -1,35 +1,41 @@
-// ignore_for_file: unused_import, prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: unused_import, prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, unused_element, unused_local_variable, unnecessary_string_interpolations, avoid_print
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class SignInPage extends StatelessWidget {
-  const SignInPage({Key? key}) : super(key: key);
+Future<void> _signInAnonymously() async {
+  try {
+    final userCredentials = await FirebaseAuth.instance.signInAnonymously();
+    print('${userCredentials.user!.uid}');
+  } catch (e) {
+    print(e.toString());
+  }
+}
 
+class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Authentication"),
       ),
-      body: _buildContext(),
+      body: _buildContent(),
     );
   }
 }
 
-Widget _buildContext() {
-  return Padding(
-    padding: const EdgeInsets.all(8),
-    child: Column(
-      children: [
-        Text(
-          "Sign in",
-          style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
-        ),
-        SizedBox(
-          height: 0,
-        ),
-        ElevatedButton(onPressed: () {}, child: Text("Go Anonymous"))
-      ],
-    ),
+Widget _buildContent() {
+  return Column(
+    children: [
+      Text(
+        "Sign in",
+        style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+      ),
+      SizedBox(
+        height: 8,
+      ),
+      ElevatedButton(
+          onPressed: () => _signInAnonymously(), child: Text("Go Anonymous"))
+    ],
   );
 }
